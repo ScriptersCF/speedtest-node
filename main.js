fa626771bd5f1027164c3e306e60fe84899948bc
Harri L <harri.liimann@gmail.com>
2019-05-13 23:04:16 +0300

    Add main files, dependencies

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const download = require('download');
+const fs = require('fs');
+// 1MB = 8Mb
+const size = 100 * 8;
+let interval = 1000 * 3600;
+
+
+setInterval(async() => {
+    let start = Date.now();
+    let d = new Date();
+    console.log('-----------------');
+    console.log('Started download');
+    const href = 'https://speed.hetzner.de/100MB.bin';
+    const filePath = '/'
+    await download(href, filePath);
+    let end = Date.now() - start;
+    let mbps = Math.round(size / (end / 1000))
+    await fs.appendFile(`./log/${d.getDate()}-${d.getMonth()+1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => {
+        if (err) throw err
+    });
+    await console.clear();
+    console.log('Finished download');
+    console.log(mbps + ' Mbps');
+    console.log('Time in seconds: ' + Math.round(end / 1000) + 's');
+    fs.unlinkSync(`${filePath}/100MB.bin`);
+}, interval)
+
