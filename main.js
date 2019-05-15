107669aa4be8f74792a8b146dabb2319a94051a1
Harri L <harri.liimann@gmail.com>
2019-05-15 21:28:48 +0300

    Added `stopInterval();` and additional documentation
    Added the `stopInterval();` function which will reset the interval after each loop.
    Some lines now include comments to explain their functions.

--- main.js
+++ main.js
@@ -5,24 +5,31 @@ const size = 100 * 8;
 let interval = 1000 * 3600;
 
 const main = async () => {
+    function stopInterval() {
+        clearInterval(loop); // Stops the loop
+        interval = Math.floor((Math.random() * 3601) + 1) * 1000; // Generates a random number between 3600 and 1
+        loop = setInterval(main, interval); // Sets the loop to run with the newly generated interval
+    }
+    
     let start = Date.now();
     let d = new Date();
     console.log('----------------');
     console.log('Started download');
     const href = 'https://speed.hetzner.de/100MB.bin';
     const filePath = './';
-    await download(href, filePath);
+    await download(href, filePath); // Download the 100MB.bin file
     let end = Date.now() - start;
     let mbps = Math.round(size / (end / 1000))
-    await fs.appendFile(`./log/${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => {
+    await fs.appendFile(`./log/${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => { // Append to log file, create one if there's none matching the current date.
         if (err) throw err;
     });
     console.clear();
     console.log('Finished download');
     console.log(`Measured speed: ${mbps} Mbps`);
     console.log(`Time in seconds: ${Math.round(end / 1000)} s`);
-    fs.unlinkSync(`${filePath}/100MB.bin`);
-    console.log('\nWaiting for next download test...')
+    fs.unlinkSync(`${filePath}/100MB.bin`); // Delete the downloaded file
+    console.log('\nWaiting for next download test...');
+    stopInterval();
 };
 main();
-setInterval(main, interval);
+let loop = setInterval(main, interval);
