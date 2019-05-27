ddd77bc26e39707bec6fd1d87bd526dbe68369d6
rebane2001 <rebane2001@gmail.com>
2019-05-27 13:37:11 +0300

    Move download URL to config file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const download = require('download');
 const { appendFile, unlinkSync } = require('fs');
-const { logPath } = require('./configurations.json');
+const config = require('./configurations.json');
 // 1MB = 8Mb
 const size = 100 * 8;
 let interval = 1000 * 3600;
@@ -14,14 +14,14 @@ function stopInterval() {
 const main = async () => {
     let start = Date.now();
     let d = new Date();
-    const href = 'https://speed.hetzner.de/100MB.bin';
+    const href = config.href;
     const filePath = './';
     console.log('----------------');
-    console.log('Started download');
+    console.log(`Downloading ${href}`);
     await download(href, filePath); // Download the 100MB.bin file
     let end = Date.now() - start;
     let mbps = Math.round(size / (end / 1000))
-    await appendFile(`${logPath}/${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => { // Append to log file, create one if there's none matching the current date.
+    await appendFile(`${config.logPath}/${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => { // Append to log file, create one if there's none matching the current date.
         if (err) throw err;
     });
     console.clear();
