b2022c9e9c79404ae90f0c9e38bf65e08cda09c9
Harri L <harri.liimann@gmail.com>
2019-11-22 16:42:00 +0200

    Update main.js
    Rewrote how size is received, thus allowing for more download files, added more download files, automatically chosen according to the speedtest results.

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 const download = require('download');
-const { appendFile, unlinkSync } = require('fs');
+const { appendFile } = require('fs');
 const config = require('./configurations.json');
-// 1MB = 8Mb
-const size = 100 * 8;
 let interval = 1000 * 3600;
+let href = config.default_href;
 
+let mbps;
 function resetTimeout() {
-    interval = Math.floor((Math.random() * 1801) + 1800) * 1000; // Generates an interval between 1 hour and 30 minutes
+    interval =  Math.floor((Math.random() * 1801) + 1800) * 1000; // Generates an interval between 1 hour and 30 minutes
+    if (mbps >= 10) {
+        href = config.decent_href;
+        
+        if (mbps >= 30) {
+            href = config.fast_href
+        }
+
+        if (mbps >= 100) {
+            href = config.superfast_href
+        }
+    }
     setTimeout(main, interval); // Sets the loop to run with the newly generated interval
 };
 
 const main = async () => {
+    let size = parseInt(href.toString().slice(27, href.toString().length - 6)) * 8;
     let start = Date.now();
     let d = new Date();
-    const href = config.href;
-    const filePath = './';
-    console.log('----------------');
+    console.log('------------------');
     console.log(`Downloading ${href}`);
-    await download(href); // Download the 100MB file, but don't save it
+    await download(href); // Download the file, but don't save it
     let end = Date.now() - start;
-    let mbps = Math.round(size / (end / 1000))
+    console.log(size)
+    mbps = Math.round(size / (end / 1000));
     await appendFile(`${config.logPath}/${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => { // Append to log file, create one if there's none matching the current date.
         if (err) throw err;
     });
