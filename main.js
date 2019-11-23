73990258868e8a6421ea4100717c50fcdc86ad17
Josh <me@joshl.io>
2019-11-23 13:03:40 +0000

    Removed debugging as per request

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ const main = async () => {
     console.log(`Downloading ${href}`);
     await download(href); // Download the file, but don't save it
     let end = Date.now() - start;
-    console.log(size)
     mbps = Math.round(size / (end / 1000));
     await appendFile(`${config.logPath}/${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => { // Append to log file, create one if there's none matching the current date.
         if (err) throw err;
