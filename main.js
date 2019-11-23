a0abd1e4f51365e171047b9e7697bfacecbda1b6
tipakA <emka456@onet.pl>
2019-11-23 21:34:53 +0100

    More linting

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const config = require('./configurations.json');
 let interval = 1000 * 3600;
 let href = config.default_href;
 let first_test = true;
-
 let mbps;
+
 function resetTimeout() {
     interval = Math.floor(Math.random() * 1801 + 1800) * 1000; // Generates an interval between 1 hour and 30 minutes
     if (first_test === true) {
@@ -28,15 +28,14 @@ function resetTimeout() {
 
 const main = async () => {
     let size = parseInt(href.toString().slice(27, href.toString().length - 6)) * 8;
-    let start = Date.now();
-    let d = new Date();
+    const d = new Date();
     if (href.toString().includes('1GB')) {
         size = 1000 * 8;
     }
     console.log('------------------');
     console.log(`Downloading ${href}`);
     await download(href); // Download the file, but don't save it
-    let end = Date.now() - start;
+    const end = Date.now() - d;
     mbps = size / (end / 1000);
     appendFile(`${config.logPath}/${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.txt`, `[${d}] ${mbps} Mbps\n`, (err) => { // Append to log file, create one if there's none matching the current date.
         if (err) throw err;
